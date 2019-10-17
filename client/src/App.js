803e335c00e312e06fa3416bb18e5077e6b599fc
Kevin Allen <kevin.allen91@gmail.com>
2019-10-17 13:57:12 -0700

    update movie posting and updating correctly

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import UpdateMovie from './Movies/updateMovie'
+import axios from 'axios'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([{}])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => setMovies({ movies: res.data }))
+      .catch(err => console.log(err.response));
+  })
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -23,7 +32,7 @@ const App = () => {
         }}
       />
       <Route path ="/update-movie/:id" render={props => {
-          return <UpdateMovie {...props} />
+          return <UpdateMovie {...props} movies={movies} UpdateMovie={setMovies} />
       }} />
     </>
   );
--- client/src/Movies/updateMovie.js
+++ client/src/Movies/updateMovie.js
@@ -1,29 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const initialMovie = {
+    name: '',
+    director: '',
+    score: '',
+    actors:''
+}
 
 const UpdateMovie = props => {
+
+    const [movie, setMovie] = useState(initialMovie)
+    useEffect(() => {
+        console.log(props)
+        const movieToEdit = props.movies.movies.find(
+            movie => `${movie.id}` === props.match.params.id
+          );
+        
+        if (movieToEdit) setMovie(movieToEdit)
+    }, [ props.match.params.id])
+
+    const changeHandler = ev => {
+        ev.persist();
+        let value = ev.target.value;
+        setMovie({
+          ...movie,
+          [ev.target.name]: value
+        });
+      };
+
+    const formSubmit = e => {
+        e.preventDefault()
+        axios
+            .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+            .then (res => {
+                console.log(res)
+                props.UpdateMovie(res.data)
+                props.history.push('/')
+            })
+            .catch(err => console.log(err.response))
+    }
 
     return (
         <div>
             <h2>Update Movie</h2>
 
-            <form className='form-container'>
+            <form className='form-container' onSubmit={formSubmit}>
                 <label className="movie-title">
                     Movie Title 
-                    <input type='text' name='title' placeholder='Title' />
+                    <input type='text' name='title' placeholder='Title' value={movie.title} onChange={changeHandler} />
                 </label>
                 <label className="movie-director">
                     Director:
-                    <input type='text' name='director' placeholder='Director' />
+                    <input type='text' name='director' placeholder='Director' value={movie.director} onChange={changeHandler} />
                 </label>
                 <label className="movie-score">
                     Metascore: 
-                    <input type='text' name='score' placeholder='Score' />
+                    <input type='text' name='score' placeholder='Score' value={movie.metascore} onChange={changeHandler} />
                 </label>
                 <label className="movie-actors">
                     Actors: 
-                    <input type='text' name='actors' placeholder='Actor 1' />
-                    <input type='text' name='actors' placeholder='Actor 2' />
-                    <input type='text' name='actors' placeholder='Actor 3' />
+                    <input type='text' name='actors' placeholder='Actor 1' value={movie.stars} onChange={changeHandler} />
+                    
                 </label>
 
                 <button className="submit-button" >Add Movie</button>
